4a59101bd5f9fa7e5f7324184361813f5db76f3c
Amy Fogelman <abfnj1@gmail.com>
2019-08-26 15:34:09 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,10 +1,7 @@
-class CreateArtists < ActiveRecord::Migration[4.2]
-  def change
-    create_table :artists do |t|
-      t.string :name
-      t.string :genre
-      t.integer :age
-      t.string :hometown
-    end
+class CreateArtists < ActiveRecord::Migration[5.2]
+  def up
+  end
+ 
+  def down
   end
 end
