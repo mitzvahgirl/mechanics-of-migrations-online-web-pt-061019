73aebd83c7097aa10d48d8dfceee36120dd40de8
Amy Fogelman <abfnj1@gmail.com>
2019-08-26 15:58:24 +0000

    Done.

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,4 +1,4 @@
-class CreateArtists < ActiveRecord::Migration[4.2]
+class CreateArtists < ActiveRecord::Migration[5.2]
   def change
     create_table :artists do |t|
       t.string :name
@@ -8,3 +8,5 @@ class CreateArtists < ActiveRecord::Migration[4.2]
     end
   end
 end
+
+  
